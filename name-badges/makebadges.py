454fc363f8d25615898f761ce777c1af03cc032e
Tyler Ward <tyler@scorpia.co.uk>
2019-02-27 22:10:12 +0000

    assorted small fixes

--- name-badges/makebadges.py
+++ name-badges/makebadges.py
@@ -2,9 +2,8 @@
 
 import sys
 import re
-import lxml.etree as ET
+import lxml.etree as etree
 import subprocess
-#from cairosvg import svg2png
 
 namesfile = sys.argv[1]
 
@@ -12,24 +11,30 @@ names=open(namesfile).readlines()
 
 def altersvg(file,name,role):
     svg = open(file, mode="rb").read()
-    root = ET.fromstring(svg)
+    root = etree.fromstring(svg)
     print(root)
     root.findall(".//text[@inkscape:label='Name']",root.nsmap)[0][0].text=name
     root.findall(".//text[@inkscape:label='Role']",root.nsmap)[0][0].text=role
-    return(ET.tostring(root, encoding='UTF-8'))
+    return(etree.tostring(root, encoding='UTF-8'))
 
 
 
-subprocess.call(["mkdir", "tmp"])
-subprocess.call(["mkdir", "out"])
+os.mkdir("tmp")
+os.mkdir("tmp")
 for name in names:
-    parts = name.strip().split(":")
+    parts = name.split(":")
     print(parts)
-    customsvg = altersvg("badge.svg",parts[0],parts[1])
+    name = parts[0].strip()
+    role = parts[1].strip()
+    customsvg = altersvg("badge.svg",name,role)
     #svg2png(bytestring=customsvg,write_t0='tmp.png')
 
-    name = parts[0]
+    if os.name == 'nt':
+        inkscape="c://Program Files/Inkscape/inkscape.exe"
+    else:
+        inkscape="inkscape"
+
     writesvg = open("tmp/"+name+".svg","wb")
     writesvg.write(customsvg)
     writesvg.close()
-    subprocess.call(["c://Program Files/Inkscape/inkscape.exe", "-f", "tmp/"+name+".svg", "-e", "out/"+name+".png", "-d", "600"])
+    subprocess.call([inkscape, "-f", "tmp/"+name+".svg", "-e", "out/"+name+".png", "-d", "600"])
